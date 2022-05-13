a193322c62ba2326c8cf4bc390709eb22c0f1ec7
Vinícius Fernandes de Souza <vinicius.fernandes2000@outlook.com>
2022-05-13 00:29:17 -0300

    recuperar senha - site

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -194,7 +194,7 @@ class User extends Model {
 
     }
 
-    public static function getForgot($email)
+    public static function getForgot($email, $inadmin = true)
     {
 
         $sql = new Sql();
@@ -233,7 +233,15 @@ class User extends Model {
 
 				$code = base64_encode($code);
 
-                $link = "http://www.viniecommerce.com.br/admin/forgot/reset?code=$code";
+                if ($inadmin === true){
+
+                    $link = "http://www.viniecommerce.com.br/admin/forgot/reset?code=$code";
+
+                } else {
+
+                    $link = "http://www.viniecommerce.com.br/forgot/reset?code=$code";
+
+                }
 
                 $mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir senha da Hcode Store", "forgot", array(
                     "name"=>$data["desperson"],
